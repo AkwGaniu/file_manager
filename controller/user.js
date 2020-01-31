fcde50e0e8dfded697bcd8ff456c9bb2f31e0ec4
AkwGaniu <lmd4sure@gmail.com>
2020-01-31 09:20:00 +0100

    fixed

--- controller/user.js
+++ controller/user.js
@@ -16,7 +16,7 @@ module.exports.register = async (req, res) => {
 
 
     //Get user details from the form and update the database
-    userModel.User.find({email: req.body.email}, {name: false, password: false}, (err, data)  => {
+    Model.User.find({email: req.body.email}, {name: false, password: false}, (err, data)  => {
         if(err) throw err
         if(data.length === 0) {
             const record = {
@@ -26,7 +26,7 @@ module.exports.register = async (req, res) => {
                 password: hashedPass
             }
 
-            userModel.User(record).save((err, data) => {
+            Model.User(record).save((err, data) => {
             if(err) throw err
                 res.status(200).json("Success")
             })
@@ -41,12 +41,12 @@ module.exports.register = async (req, res) => {
 module.exports.login = async (req, res) => {
     //Get user details from the login page and validate the login
     const user = await Model.user.findOne({email: req.body.email})
-    if (!user) return res.status(404).json("Invalid email")
+    if (!user) return res.status(401).json("Invalid email")
 
     // CONFIRM USER PASSWORD
     const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword) return res.status(404).json("Invalid passsword")
-    res.json({
+    if (!validPassword) return res.status(401).json("Invalid passsword")
+    res.status(200).json({
         name: user.name,
         email: user.email,
         userId: user.userId
